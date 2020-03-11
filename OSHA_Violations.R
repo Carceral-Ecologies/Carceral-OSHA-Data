c1cd8c0db7717258be82628172be62bfa4382ce3
savannahmhunter <shunter@ucdavis.edu>
2020-03-10 20:34:53 -0700

    Updated merge_isolate and subset violations

---
+++ OSHA_Violations.R
@@ -0,0 +1,20 @@
+#### Purpose ####
+# The purpose of this code is to take activity_nr numbers from the OSHA inspections dataset and locate them in the violations dataset. 
+
+inspections = read.csv("prison_inspections_2010.csv")
+
+
+#### Load in OSHA Inspection Datasets ####
+vfiles = list.files("Data/Violations", full.names = TRUE)
+violations = lapply(vfiles, read.csv, header = TRUE, stringsAsFactors = FALSE)
+violations_df = do.call(rbind, violations)
+
+activity_nr_char = as.integer(inspections$activity_nr)
+prison_violations = violations_df[violations_df$activity_nr %in% activity_nr_char, ]
+
+# Checking if any prisons are not in the violations_df
+no_viol = inspections[!(inspections$activity_nr %in% violations_df$activity_nr), "activity_nr"]
+#809 prisons with no violations 
+
+# save 
+write.csv(prison_violations, file = "prison_violations_2010.csv")
