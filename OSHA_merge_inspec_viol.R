238c480ccd8ecbd2da56c10382c17425fbbe09ac
savannahmhunter <shunter@ucdavis.edu>
2020-05-26 19:23:58 -0700

    Included facilities that have inspection info but not violation info. Before they were dropped.

--- OSHA_merge_inspec_viol.R
+++ OSHA_merge_inspec_viol.R
@@ -1,4 +1,5 @@
-# Merging inspection and violation datasets 
+#### Purpose ####
+# This code merges the inspection and violation datasets 
 
 
 viol1 = read.csv(file = "Cleaned_Data/prison_violations_2010.csv", header = TRUE, stringsAsFactors = FALSE)
@@ -6,6 +7,7 @@ viol1 = read.csv(file = "Cleaned_Data/prison_violations_2010.csv", header = TRUE
 inspec = read.csv(file = "Cleaned_Data/prison_inspections_2010.csv", header = TRUE, stringsAsFactors = FALSE)
 
 
-final = merge(inspec, viol1, by = "activity_nr", all.y = TRUE)
+final = merge(inspec, viol1, by = "activity_nr", all.x = TRUE)
+# All.x = assures that inspections with no matching violations entry will be included in the final dataset 
 
 write.csv(final, file = "Cleaned_Data/prison_insp_viol_2010.csv")
