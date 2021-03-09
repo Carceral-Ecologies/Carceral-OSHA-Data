aa66ab6f4b06157d54f48559794039b5356f35f5
savannahmhunter <shunter@ucdavis.edu>
2021-03-09 12:48:18 -0800

    delete files to reorganized repo

--- CA_prison subset code.R
+++
@@ -1,7 +0,0 @@
-library(dplyr)
-
-CA_prison_insp_viol_2010_op <- prison_insp_viol_2010_op %>% filter_at(vars(site_state), any_vars(. %in% 'CA'))
-
-CA_prison_insp_viol_2010_op
-
-write.csv(CA_prison_insp_viol_2010_op, 'CA_prison_insp_viol_2010_op.csv')
